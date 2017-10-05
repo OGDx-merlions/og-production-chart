109f41e4b99db6f85b6ba143f074919443baf06f
Ezhil Vendhan <ezhilvendhan.vezhavendhan@ge.com>
2017-10-05 11:24:38 +0800

    Fix attributes, support single chart

--- dist/og-production-chart.js
+++ dist/og-production-chart.js
@@ -1,4 +1,4 @@
-"use strict";(function(){Polymer({is:"og-production-chart",properties:{/**
+"use strict";(function(){Polymer({is:"og-production-chart",listeners:{},properties:{/**
       * This property is a list of tab labels
       *
       * @property labels
@@ -15,22 +15,19 @@
       *
       * @property height
       */height:{type:Number,value:300},/**
-      * The Actual value to be displayed in the legend
+      * The Actual, Target and design values to be displayed in the legend
       *
-      * @property actualDispVal
-      */actualDispVal:{type:Number},/**
-      * The Target value to be displayed in the legend
-      *
-      * @property targetDispVal
-      */targetDispVal:{type:Number},/**
-      * The Design value to be displayed in the legend
-      *
-      * @property designDispVal
-      */designDispVal:{type:Number},/**
+      * @property legendDispVal
+      [{"actual": 1302, "target": 1212, "design": 1400}]
+      */legendDispVal:{type:Array,value:function value(){return[]}},/**
 			* This property is an Array of tab data.
 			Member of each tab data looks like this
 			`{"date":"25-Apr-17","actual":"11960","target":"12950","design":"13960"}`
       *
-      * @property counterValue
-      */data:{type:Array,value:function value(){return[]}},selected:{type:Number,value:0}}})})();
+      * @property data
+      */data:{type:Array,value:function value(){return[]},observer:"_setLegend"},selected:{type:Number,value:0}},_setLegend:function _setLegend(data){var me=this;data.forEach(function(_item,idx){_item.legend=me.legendDispVal[idx]})},attached:function attached(){this._notifyAttached()},_notifyAttached:function _notifyAttached(){/**
+       * Event fired when the component is attached
+       *
+       * @event attached
+       */this.fire("attached",{})},_isMultipleData:function _isMultipleData(data){return data.length>1},_isSingleData:function _isSingleData(data){return data.length===1}})})();
 //# sourceMappingURL=og-production-chart.js.map
--- og-production-chart.es6.js
+++ og-production-chart.es6.js
@@ -1,7 +1,11 @@
 (function() {
   Polymer({
 
-    is: 'og-production-chart', 
+    is: 'og-production-chart',
+
+    listeners: {
+
+    },
 
     properties: {
       /**
@@ -45,46 +49,63 @@
         value: 300
 			},
 			/**
-      * The Actual value to be displayed in the legend
+      * The Actual, Target and design values to be displayed in the legend
       *
-      * @property actualDispVal
+      * @property legendDispVal
+      [{"actual": 1302, "target": 1212, "design": 1400}]
       */
-			actualDispVal: {
-				type: Number
-			},
-			/**
-      * The Target value to be displayed in the legend
-      *
-      * @property targetDispVal
-      */
-			targetDispVal: {
-				type: Number
-			},
-			/**
-      * The Design value to be displayed in the legend
-      *
-      * @property designDispVal
-      */
-			designDispVal: {
-				type: Number
+			legendDispVal: {
+				type: Array,
+        value() {
+					return [];
+				}
 			},
 			/**
 			* This property is an Array of tab data.
 			Member of each tab data looks like this
 			`{"date":"25-Apr-17","actual":"11960","target":"12950","design":"13960"}`
       *
-      * @property counterValue
+      * @property data
       */
 			data: {
 				type: Array,
 				value() {
-					return []
-				}
+					return [];
+				},
+        observer: "_setLegend"
 			},
 			selected: {
 				type: Number,
 				value: 0
 			}
-		}
+		},
+
+    _setLegend(data) {
+      let me = this;
+      data.forEach((_item, idx) => {
+        _item.legend = me.legendDispVal[idx];
+      });
+    },
+
+    attached() {
+      this._notifyAttached();
+    },
+
+    _notifyAttached() {
+      /**
+       * Event fired when the component is attached
+       *
+       * @event attached
+       */
+      this.fire('attached', {});
+    },
+
+    _isMultipleData(data) {
+      return data.length > 1;
+    },
+
+    _isSingleData(data) {
+      return data.length === 1;
+    }
   });
 })();
